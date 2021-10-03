40d2d79111cc6e5bb98871471b21cae5667a7764
YZR0009 <yzr0009@tigermail.auburn.edu>
2021-10-03 16:48:50 -0400

    add search notes api

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ const logger =  require("morgan");
 const notesRouter = require("./routes/notes");
 const archiveRouter = require("./routes/archive");
 const labelsRouter = require("./routes/labels");
+const searchRouter = require("./routes/search");
 
 const port = process.env.PORT || 3001;
 
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/noteDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const URL = 'mongodb://localhost:27017/noteDB'
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
+ 
+//mongoose.Promise = global.Promise;
+mongoose.connect(URL, options);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
@@ -27,6 +37,7 @@ app.use(logger('dev'));
 app.use("/notes", notesRouter);
 app.use("/labels", labelsRouter);
 app.use("/archive", archiveRouter);
+app.use("/search", searchRouter);
 
 app.listen(port, () => {
     console.log("Dev-Note back end is running on " + port);
--- models/note.js
+++ models/note.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const noteSchema = new mongoose.Schema({
+var noteSchema = new mongoose.Schema({
   title: String,
   content: { type: String, required: true },
   archive: { type: Boolean, default: false },
   labels: [{type: String}]
 });
+
+noteSchema.index({ title : "text"})
 
 const NoteModel = mongoose.model("Note", noteSchema);
 
